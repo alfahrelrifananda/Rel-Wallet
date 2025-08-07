29baa67fdc85053b62182a044b7c0cdb61a3a57e
alfahrelrifananda <pahrel1234@gmail.com>
2025-08-07 13:58:42 +0700

    ra

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,17 +1,24 @@
+import AntDesign from "@expo/vector-icons/AntDesign";
 import Entypo from "@expo/vector-icons/Entypo";
+import Feather from "@expo/vector-icons/Feather";
 import Ionicons from "@expo/vector-icons/Ionicons";
 import { Tabs } from "expo-router";
 import * as React from "react";
 
 export default function TabsLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+    <Tabs screenOptions={{ tabBarActiveTintColor: "black" }}>
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
-          tabBarIcon: ({ color }) => {
-            return <Entypo name="home" size={24} color={color} />;
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <Entypo name="home" size={24} color={color} />
+            ) : (
+              <AntDesign name="home" size={24} color={color} />
+            );
           },
         }}
       />
@@ -19,8 +26,13 @@ export default function TabsLayout() {
         name="settings"
         options={{
           title: "Settings",
-          tabBarIcon: ({ color }) => {
-            return <Ionicons name="settings" size={24} color={color} />;
+          tabBarShowLabel: false,
+          tabBarIcon: ({ color, focused }) => {
+            return focused ? (
+              <Ionicons name="settings" size={24} color={color} />
+            ) : (
+              <Feather name="settings" size={24} color="black" />
+            );
           },
         }}
       />
---
+++ lib/appwrite.tsx
@@ -0,0 +1,5 @@
+import { Client, Databases, Account } from "react-native-appwrite";
+
+const client = new Client()
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject("https://fra.cloud.appwrite.io/v1");
